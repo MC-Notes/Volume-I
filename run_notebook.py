bccb0b2f57ff758f6642c973f24cc4ecfb7cd6ee
mzwiessele <ibinbei@gmail.com>
2016-11-18 10:52:16 +0000

    fix: travis run notebook

--- run_notebook.py
+++ run_notebook.py
@@ -17,23 +17,18 @@ PROFILE = 0
 
 def main(argv=None):
     argv = sys.argv[1:]
-    try:
-        import nbformat
-        from nbconvert.preprocessors import ExecutePreprocessor
-        notebook = argv[0]
-        note_folder = os.path.dirname(notebook)
-        with open(notebook) as f:
-            nb = nbformat.read(f, as_version=4)
-        print('Running notebook {} ...'.format(argv[0]))
-        ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
-        ep.preprocess(nb, {'metadata': {'path': note_folder}})
+    import nbformat
+    from nbconvert.preprocessors import ExecutePreprocessor
+    notebook = argv[0]
+    note_folder = os.path.dirname(notebook)
+    with open(notebook) as f:
+        nb = nbformat.read(f, as_version=4)
+    print('Running notebook {} ...'.format(argv[0]))
+    ep = ExecutePreprocessor(timeout=600, kernel_name='python3')
+    ep.preprocess(nb, {'metadata': {'path': note_folder}})
 
-        with open('{}/executed_notebook.ipynb'.format(note_folder), 'wt') as f:
-            nbformat.write(nb, f)
-
-    except Exception as e:
-        print(e)
-        return 2
+    with open('{}/executed_notebook.ipynb'.format(note_folder), 'wt') as f:
+        nbformat.write(nb, f)
 
 
 if __name__ == "__main__":
