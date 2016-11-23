f9e1207fb2b8f4d1609f2a2c7231ba27781d3309
mzwiessele <ibinbei@gmail.com>
2016-11-23 14:17:27 +0000

    fix: tell github the username and email

--- run_notebooks.sh
+++ run_notebooks.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    REPO=`git config remote.origin.url`;
+    SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:};
+    SHA=`git rev-parse --verify HEAD`;
+    ENCRYPTION_LABEL="$encrypted_a3a89bfc08a4";
+    COMMIT_AUTHOR_EMAIL="ibinbei@gmail.com";
+    openssl aes-256-cbc -K $encrypted_a3a89bfc08a4_key -iv $encrypted_a3a89bfc08a4_iv -in github_deploy.enc -out github_deploy -d;
+    chmod 600 github_deploy;
+    eval `ssh-agent -s`;
+    ssh-add github_deploy;
+    git clone $REPO out
+    cd out
+    git config user.name "Travis CI";
+    git config user.email "$COMMIT_AUTHOR_EMAIL";
+fi;
+
 for folder in $( ls -d */ )
 do
     echo $folder
@@ -24,4 +40,8 @@ do
         fi;
     done;
 done;
-	   
+
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    git push $SSH_REPO "$TRAVIS_BRANCH";
+fi
