ff6ff8a36751d55c6abda31b5764ac027b18a60e
mzwiessele <ibinbei@gmail.com>
2016-11-23 13:01:17 +0000

    fix: notebooks where not added when executed

--- run_notebooks.sh
+++ run_notebooks.sh
@@ -2,14 +2,23 @@
 
 for folder in $( ls -d */ )
 do
-	echo $folder
-	for notebook in $( ls $folder/*.ipynb )
-	do
-		if [ -a $folder/requirements.txt ]
-	    then
-	    	pip install -r $folder/requirements.txt;
-	    fi;
-	   	python run_notebook.py $notebook;
-	done;
+    echo $folder
+    for notebook in $( ls $folder/*.ipynb )
+    do
+        if [ -a $folder/requirements.txt ]
+        then
+            pip install -r $folder/requirements.txt;
+        fi;
+        if [ -a $folder/executed_notebook.ipynb ] # Only run if not already:
+        then
+            echo Running notebook $notebook...;
+            python run_notebook.py $notebook;
+            echo Adding exectued notebook to github...;
+            git add $folder/executed_notebook.ipynb;
+            git commit -m "new: Executed notebook $notebook";
+        else
+            echo Notebook $notebook already run, not rerunning.;
+        fi;
+    done;
 done;
 	   
